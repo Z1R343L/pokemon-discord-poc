fc61a4ced54d4f7e54495d8d3764c581c502ba66
jan2705 <jan2705@outlook.de>
2021-04-26 03:21:41 +0200

    settings

--- src/main/java/fr/reminy/pokemon_discord/Settings.java
+++ src/main/java/fr/reminy/pokemon_discord/Settings.java
@@ -1,10 +1,10 @@
 package fr.reminy.pokemon_discord;
 
 public class Settings {
-    public final static String PREFIX = "a!";
+    public final static String PREFIX = ",";
     public static final int CAMERA_WIDTH = 15;
     public static final int CAMERA_HEIGHT = 10;
-    public static final int SCALE_FACTOR = 2;
+    public static final int SCALE_FACTOR = 1;
     public static final int ID_COLLISIONS = 3;
     public static final int ID_EVENTS = 4;
 }
