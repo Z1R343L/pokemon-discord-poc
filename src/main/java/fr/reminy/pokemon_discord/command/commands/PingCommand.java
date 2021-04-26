cd08a3fdb5a7521e222065920e8ced6a29f76054
jan2705 <jan2705@outlook.de>
2021-04-26 21:25:26 +0200

    ping embed

--- src/main/java/fr/reminy/pokemon_discord/command/commands/PingCommand.java
+++ src/main/java/fr/reminy/pokemon_discord/command/commands/PingCommand.java
@@ -5,8 +5,10 @@ import fr.reminy.pokemon_discord.command.Command;
 import fr.reminy.pokemon_discord.annotation.command.Register;
 import org.javacord.api.entity.channel.TextChannel;
 import org.javacord.api.entity.message.MessageAuthor;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.event.message.MessageEvent;
 
+import java.awt.*;
 import java.util.List;
 
 @Register
@@ -29,6 +31,9 @@ public class PingCommand implements Command {
 
     @Override
     public void execute(MessageEvent event, MessageAuthor author, TextChannel channel, List<String> args) {
-        channel.sendMessage("Pong!");
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+        embedBuilder.setColor(Color.WHITE);
+        embedBuilder.setDescription("Pong!");
+        channel.sendMessage(embedBuilder);
     }
 }
